ed9b5d2eeab7fd499e31a058c2c6950b124c12c5
Xaver Schick <Xaver.schick@web.de>
2020-12-08 13:08:15 +0100

    Working on method isMagicSquare() in class Main. Adding comments

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,30 +1,34 @@
 import java.util.Arrays;
 
-public class MagicSquare {
-    public MagicSquare(int[][] square){
+public class MagicSquare {#
+    int[][] magicField;
 
+    public MagicSquare(int[][] square) {
+        this.magicField = square;
     }
-    //public boolean isMagicSquare(){}
 
-    /**This method calculates all magic numbers up to the entered edge length k.
+    public boolean isMagicSquare() {
+        return a;
+    }
+    public boolean isSemimagicSquare() {
+        return b;
+    }
+    /**
+     * This method calculates all magic numbers up to the entered edge length k.
      * All the results are saved in a specially generated array.
-     * When all magic numbers are calculated this array is converted to a String variable (output) for return**/
-    static String showMagicNumbers(int k){
+     * When all magic numbers are calculated this array is converted to a String variable (output) for return
+     **/
+    static String showMagicNumbers(int k) {
         int[] magicNumbers = new int[k];
         int p = 0;
-        for (int i=0; i<k; i++){
-            p =p+1;
-            magicNumbers[i] = ((p*p*p)+p)/2; //Calculate the magic number and store it in the array
+        for (int i = 0; i < k; i++) {
+            p = p + 1;
+            magicNumbers[i] = ((p * p * p) + p) / 2; //Calculate the magic number and store it in the array
         }
         String output = Arrays.toString(magicNumbers); //Convert the array to a String
-        output = output.substring(1, output.length()-1); //Cut the two square brackets
+        output = output.substring(1, output.length() - 1); //Cut the two square brackets
         return output;
     }
-   /* public static double calcMagicNumber(int n){
-        double sum = ((n*n*n)+1)/2;
-        return sum;
-    }*/
-    //public boolean isSemimagicSquare(){}
     //public void complement(){}
     //public String toString(){}
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
-    private static String testOperation = "showMagicNumbers";
+    private static String testOperation = "isMagicSquare?";
     Scanner scan = new Scanner(System.in);
     private static String test = "4,9,2;3,5,7;8,1,6";
     private static String testeingabe = "9";
@@ -11,16 +12,30 @@ public class Main {
     public static void main(String[] args) {
         switch (testOperation) {
             case "showMagicNumbers":
-                System.out.println(MagicSquare.showMagicNumbers(Integer.valueOf(testeingabe))); //Outputs the magic numbers to the given n (args[1])
+                System.out.println(MagicSquare.showMagicNumbers(Integer.parseInt(testeingabe))); //Outputs the magic numbers to the given n (args[1])
                 break;
             case "isMagicSquare?":
                 //MagicSquare magicSquareObj = new MagicSquare(args[])
-                /*String[] newArray = test.split(";");
+                String[] rowArray = test.split(";"); //Splittung input and write it to an array
+                int[][] magicSquareField = new int[rowArray.length][rowArray.length]; //Initializing new 2D Array to use it as Magic Square Field
+                for (int i=0;i<rowArray.length;i++){
+                    String[] columArray = rowArray[i].split(","); //Splitting another time to have the individual numbers and write it to an array
+                    for (int x=0;x<rowArray.length;x++){
+                        magicSquareField[i][x] = Integer.parseInt(columArray[x]); //write the whole input to the rows and colums of the 2D array
+                    }
+                }
+                MagicSquare field1 = new MagicSquare(magicSquareField); //New MagicSquare object
+                /* Testing whether the field is a magic square, a semimagic square or not magical */
+                if (field1.isMagicSquare()){
+                    System.out.println("magic square");
+                }
+                if (field1.isSemimagicSquare()){
+                    System.out.println("semimagic square");
+                }
+                if (!(field1.isMagicSquare())||!(field1.isSemimagicSquare())){
+                    System.out.println("not magical");
+                }
 
-                System.out.println(newArray.length);
-                for (int i=0; i < newArray.length; i++){
-                    System.out.println(newArray[i]);
-                }*/
                 break;
             case "complement":
 
