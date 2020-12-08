cb200f2ec5bc6a70dfa60ae53feb52c2c6254c03
Xaver Schick <Xaver.schick@web.de>
2020-12-08 11:33:18 +0100

    Implementing method ShowMagicNumbers to MagicSquare class to use the operation showMagicNumbers.

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,13 +1,27 @@
-/*public class MagicSquare {
+import java.util.Arrays;
+
+public class MagicSquare {
     public MagicSquare(int[][] square){
 
     }
-    public boolean isMagicSquare(){}
+    //public boolean isMagicSquare(){}
     static String showMagicNumbers(int k){
-
+        int[] magicNumbers = new int[k];
+        int p = 0;
+        for (int i=0; i<k; i++){
+            p =p+1;
+            magicNumbers[i] = ((p*p*p)+p)/2;
+        }
+        String output = Arrays.toString(magicNumbers);
+        output = output.substring(1, output.length()-1);
+        return output;
     }
-    public boolean isSemimagicSquare(){}
-    public void complement(){}
-    public String toString(){}
+   /* public static double calcMagicNumber(int n){
+        double sum = ((n*n*n)+1)/2;
+        return sum;
+    }*/
+    //public boolean isSemimagicSquare(){}
+    //public void complement(){}
+    //public String toString(){}
 
-}*/
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
+
 public class Main {
     private static String testOperation = "showMagicNumbers";
     Scanner scan = new Scanner(System.in);
     private static String test = "4,9,2;3,5,7;8,1,6";
+    private static String testeingabe = "9";
     //public int
 
     public static void main(String[] args) {
         switch (testOperation) {
             case "showMagicNumbers":
-                String[] newArray = test.split(";");
-                System.out.println(newArray.length);
-                for (int i=0; i < newArray.length; i++){
-                    System.out.println(newArray[i]);
-                }
-
-               //MagicSquare.showMagicNumbers((args[1]).length());
+                System.out.println(MagicSquare.showMagicNumbers(Integer.valueOf(testeingabe)));
                 break;
             case "isMagicSquare?":
                 //MagicSquare magicSquareObj = new MagicSquare(args[])
+                /*String[] newArray = test.split(";");
+
+                System.out.println(newArray.length);
+                for (int i=0; i < newArray.length; i++){
+                    System.out.println(newArray[i]);
+                }*/
                 break;
             case "complement":
 
